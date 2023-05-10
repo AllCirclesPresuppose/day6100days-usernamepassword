048fa2857585c9cf5b4de0cfa921d0863890d0aa
AllCirclesPresuppose <114363778+AllCirclesPresuppose@users.noreply.github.com>
2023-05-10 19:28:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+username = input("What is your username?")
+password = input("What is your password?")
+
+if username == "circles" and password == "presuppose":
+    print("Hello Circles! may all your presuppositions end favorably!")
+    print("Have a lovely day, and continue your coding!")
+elif username == "bundy" and password == "rnr":
+    print("Hello Bundy! Keep up the good work on the studio!")
+    print("The music is sounding great!")
+elif username == "jones" and password == "dao":
+    print("Hello Dao Jones! how is our favorite flightless bird?")
+    print("Enjoy the penguins, and go buy a cute plushie!")
+else:
+    print(
+        "Sorry, username/password combination not found. Please enter correct username/password"
+    )
